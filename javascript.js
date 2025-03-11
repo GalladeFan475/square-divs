217ac75f72f5dbc9064d39d845c1a8e422d1f030
GalladeFan475 <smashingsanjay6@gmail.com>
2025-03-11 17:58:57 +0800

    final change

--- javascript.js
+++ javascript.js
@@ -1,3 +1,21 @@
+//Create Text
+const textDiv = document.createElement("div");
+const text = document.createElement("h1");
+text.textContent = "Etch-a-Sketch!"
+textDiv.style.display = "flex";
+textDiv.style.justifyContent = "center";
+textDiv.style.alignItems = "center";
+text.style.padding = "20px";
+text.style.fontFamily = "Arial, sans-serif";
+text.style.fontSize = "50px";
+text.style.fontWeight = "bold";
+text.style.color = "yellow";
+text.style.backgroundColor = "black"; 
+text.style.fontStyle = "italic";
+textDiv.appendChild(text);
+//-----------------------------------
+
+//Create Container to store squares
 const containerDiv = document.createElement("div");
 const container = document.createElement("div");
 container.style.width = "600px";
@@ -6,11 +24,9 @@ containerDiv.appendChild(container);
 containerDiv.style.display = "flex";
 containerDiv.style.justifyContent = "center";
 containerDiv.style.alignItems = "center";
+//------------------------------
 
-
-document.body.style.margin = "0";
-document.body.style.padding = "0";
-
+//Create Button
 const btnDiv = document.createElement("div");
 const btn = document.createElement("button");
 btn.style.padding = "20px";
@@ -20,7 +36,9 @@ btnDiv.appendChild(btn);
 btnDiv.style.display = "flex";
 btnDiv.style.justifyContent = "center";
 btnDiv.style.alignItems = "center";
+//-------------------------------
 
+//Click Button
 btn.addEventListener("click", () =>{
     let condition = parseInt(prompt("How many squares do you want? "));
 
@@ -57,6 +75,12 @@ btn.addEventListener("click", () =>{
         container.appendChild(row);
     }
 })
+//--------------------------------------
 
+//Adding Divs to body
+document.body.style.margin = "0";
+document.body.style.padding = "0";
+document.body.appendChild(textDiv);
 document.body.appendChild(btnDiv);
 document.body.appendChild(containerDiv);
+//----------------------------------------

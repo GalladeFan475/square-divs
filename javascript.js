6f2a3d5b77b2d1dd90aaa59b91ecc2369e0f969d
GalladeFan475 <smashingsanjay6@gmail.com>
2025-03-11 10:24:36 +0800

    added 16x16

--- javascript.js
+++ javascript.js
@@ -1,21 +1,23 @@
 const container = document.createElement("div");
-
-
-const row = document.createElement("div");
-row.style.display = "flex";
-row.style.justifyContent = "center"; 
-row.style.alignItems = "center";
+document.body.style.margin = "0";
+document.body.style.padding = "0";
 
 for(let i = 0; i < 16; i++){
-    const newDiv = document.createElement("div");
-    newDiv.style.width = `${100 / 16}vw`;
-    newDiv.style.aspectRatio = "1 / 1";
-    newDiv.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, 
+    const row = document.createElement("div");
+    row.style.display = "flex";
+    row.style.justifyContent = "center"; 
+    row.style.alignItems = "center";
+
+    for(let i = 0; i < 16; i++){
+        const newDiv = document.createElement("div");
+        newDiv.style.width = `${100 / 16}vw`;
+        newDiv.style.aspectRatio = "1 / 1";
+        newDiv.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, 
                                         ${Math.floor(Math.random() * 256)}, 
                                         ${Math.floor(Math.random() * 256)})`
-    row.appendChild(newDiv);
+        row.appendChild(newDiv);
+        container.appendChild(row);
+    }
 }
-
-container.appendChild(row);
 document.body.appendChild(container);
 
